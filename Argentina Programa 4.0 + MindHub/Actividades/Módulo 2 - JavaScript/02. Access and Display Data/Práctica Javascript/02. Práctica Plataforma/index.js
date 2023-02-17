672ff51858e04f41f28f17c72a7536215d30bd53
Milton <miltonfruizok@outlook.es>
2023-02-17 15:01:44 -0300

    Creación carpeta Práctica Plataforma

---
+++ Argentina Programa 4.0 + MindHub/Actividades/Módulo 2 - JavaScript/02. Access and Display Data/Práctica Javascript/02. Práctica Plataforma/index.js
@@ -0,0 +1,24 @@
+// 01. Crea una variable que contenga tu nombre
+const name = "Milton";
+
+// 02. Crea una variable que contenga tu edad
+const age = 33;
+
+// 03. Crea una variable que indique si estas en pareja (T/F)
+const couple = false;
+
+// 04. Crea una variable que contenga un array con los números del 1 al 10
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// 05. Crea una variable que contenga un array con los nombres de 5 colegas de la cursada
+const classMates = ["Lilo", "Gbdiaz", "Mauri Luque", "Mika Chervet", "Ra!"];
+
+// 06. Crea un objeto que contenga el valor de todas las variables anteriores
+const object = {
+  name: "Milton",
+  age: 33,
+  isInCouple: false,
+  makeACount: array,
+  hasClassMates: classMates,
+};
+// console.log("Muestro el objeto: ", object);
