67151be4605e0e5d03fc64d03a609042a8fc7c3a
Milton <miltonfruizok@outlook.es>
2023-03-20 12:49:13 -0300

    Modificación div tabla

--- Argentina Programa 4.0 + MindHub/Actividades/Módulo 2 - JavaScript/04. Access Live Data/01. Task 4/assets/scripts/stats.js
+++ Argentina Programa 4.0 + MindHub/Actividades/Módulo 2 - JavaScript/04. Access Live Data/01. Task 4/assets/scripts/stats.js
@@ -21,7 +21,7 @@ async function fetchDataStats() {
     const highPercentage = assistanceArray
       .sort((arrayA, arrayB) => arrayB.percentage - arrayA.percentage)
       .map((array) => {
-        return `${array.name}: ${array.percentage}%`;
+        return `${array.name} ${array.percentage}%`;
       });
 
     const lowPercentage = highPercentage.slice(-3);
@@ -33,21 +33,21 @@ async function fetchDataStats() {
     const highCapacity = capacityPast
       .sort((arrayA, arrayB) => arrayB.capacity - arrayA.capacity)
       .map((array) => {
-        return `${array.name}: ${array.capacity}`;
+        return `${array.name} ${array.capacity} people`;
       });
     const eventStatistics = (document.getElementById(
       "events-statistics"
-    ).innerHTML = `<tr>
+    ).innerHTML = `<tr class="rowPointer"> 
                     <td>${highPercentage[0]}</td>
                     <td>${lowPercentage[2]}</td>
                     <td>${highCapacity[0]}</td>
                   </tr>
-                  <tr>
+                  <tr class="rowPointer">
                     <td>${highPercentage[1]}</td>
                     <td>${lowPercentage[1]}</td>
                     <td>${highCapacity[1]}</td>
                   </tr>
-                  <tr>
+                  <tr class="rowPointer">
                     <td>${highPercentage[2]}</td>
                     <td>${lowPercentage[0]}</td>
                     <td>${highCapacity[2]}</td>
@@ -107,32 +107,32 @@ async function fetchDataStats() {
     const upcomingEventsStatistics = (document.getElementById(
       "upcoming-events-statistics"
     ).innerHTML = `
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[0]}</td>
             <td>$ ${arrayUpcoming[0][0]}</td>
             <td>${arrayUpcoming[0][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[1]}</td>
             <td>$ ${arrayUpcoming[1][0]}</td>
             <td>${arrayUpcoming[1][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[2]}</td>
             <td>$ ${arrayUpcoming[2][0]}</td>
             <td>${arrayUpcoming[2][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[3]}</td>
             <td>$ ${arrayUpcoming[3][0]}</td>
             <td>${arrayUpcoming[3][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[4]}</td>
             <td>$${arrayUpcoming[4][0]}</td>
             <td>${arrayUpcoming[4][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${upcomingCategories[5]}</td>
             <td>$${arrayUpcoming[5][0]}</td>
             <td>${arrayUpcoming[5][1]} %</td>
@@ -188,37 +188,37 @@ async function fetchDataStats() {
     const pastEventsStatistics = (document.getElementById(
       "past-events-statistics"
     ).innerHTML = `
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[0]}</td>
             <td>$ ${arrayPast[0][0]}</td>
             <td>${arrayPast[0][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[1]}</td>
             <td>$ ${arrayPast[1][0]}</td>
             <td>${arrayPast[1][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[2]}</td>
             <td>$ ${arrayPast[2][0]}</td>
             <td>${arrayPast[2][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[3]}</td>
             <td>$ ${arrayPast[3][0]}</td>
             <td>${arrayPast[3][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[4]}</td>
             <td>$ ${arrayPast[4][0]}</td>
             <td>${arrayPast[4][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[5]}</td>
             <td>$ ${arrayPast[5][0]}</td>
             <td>${arrayPast[5][1]} %</td>
         </tr>
-        <tr>
+        <tr class="rowPointer">
             <td>${pastCategories[6]}</td>
             <td>$ ${arrayPast[6][0]}</td>
             <td>${arrayPast[6][1]} %</td>
