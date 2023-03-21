5e557cb6927d4c82263cb6c253232458f3a3e74d
Milton <miltonfruizok@outlook.es>
2023-03-21 18:30:09 -0300

    Modificación archivo home.js

--- Argentina Programa 4.0 + MindHub/Actividades/Módulo 2 - JavaScript/04. Access Live Data/01. Task 4/assets/scripts/home.js
+++ Argentina Programa 4.0 + MindHub/Actividades/Módulo 2 - JavaScript/04. Access Live Data/01. Task 4/assets/scripts/home.js
@@ -1,150 +1,159 @@
-//------------------------------------------------------------------- API --------------------------------------------------------------------//
+const apiHome = "https://mindhub-xj03.onrender.com/api/amazing";
 
-let api = "https://mindhub-xj03.onrender.com/api/amazing";
-
-async function fetchData() {
+async function fetchDataHome() {
   try {
-    const response = await fetch(api);
+    const response = await fetch(apiHome);
     const homeData = await response.json();
 
-    const homeContainer = document.getElementById("home-container");
-    const finderContainerHome = document.getElementById(
-      "finder-container-home"
-    );
-    const formContainerHome = document.getElementById("form-container-home");
-    const categoryContainerHome = document.getElementById(
-      "category-container-home"
-    );
-    const foodCheckbox = document.getElementById("food-checkbox");
-
-    const homeCards = homeData.events
-      .map((cards) => enabledCard(cards))
-      .join("");
-    homeContainer.innerHTML = homeCards;
-    formContainerHome.addEventListener("keyup", (event) => {
-      event.preventDefault();
-      if (event.key === "Escape") finderContainerHome.value = "";
-      if (event.key === "Enter")
-        finderContainerHome.value === finderContainerHome;
-      let nameFilter = homeData.events
-        .filter((parameter) =>
-          parameter.name
-            .toLowerCase()
-            .includes(finderContainerHome.value.toLowerCase())
-        )
-        .map((card) => enabledCard(card))
-        .join("");
-      homeContainer.innerHTML = nameFilter;
-      if (!nameFilter) {
-        wrongCard;
-        homeContainer.innerHTML = wrongCard;
+    let arrayCategory = homeData.events.map((array) => array.category);
+    let categoryFiltering = arrayCategory.reduce((array, element) => {
+      if (!array.includes(element)) {
+        array.push(element);
       }
+      return array;
+    }, []);
+
+    //---------------------------------------------------------------- Capture ---------------------------------------------------------------//
+    let homeContainer = document.getElementById("home-container");
+    let homeCategory = document.getElementById("home-category");
+    const searchInput = document.getElementById("search-input");
+
+    //----------------------------------------------------------- Arrays / Variables ---------------------------------------------------------//
+    let propertySearch = "";
+    let searchFilter = [];
+    let checked = [];
+    let categoryCheck = [];
+
+    //----------------------------------------------------------------- Search ---------------------------------------------------------------//
+    searchInput.addEventListener("keyup", (event) => {
+      propertySearch = event.target.value.toLowerCase();
+      filterFunction();
     });
 
-    formContainerHome.addEventListener("submit", (event) => {
-      event.preventDefault();
-      finderContainerHome.value;
-      finderContainerHome === finderContainerHome.value;
-    });
-    let categories = "";
-    homeData.events.map((parameter) => {
-      if (!categories.includes(parameter.category)) {
-        categories += enableCheckbox(parameter);
+    //---------------------------------------------------------------- Check Box -------------------------------------------------------------//
+    homeCategory.addEventListener("click", (event) => {
+      if (event.target.checked) {
+        checked.push(event.target.value);
+      } else {
+        checked = checked.filter(
+          (notChecked) => notChecked !== event.target.value
+        );
       }
+      filterFunction();
     });
-    categoryContainerHome.innerHTML = categories;
 
-    let active = [];
-    categoryContainerHome.addEventListener("click", (events) => {
-      if (events.target.checked != undefined) {
-        if (events.target.checked) {
-          let create = homeData.events
-            .filter(
-              (argument) =>
-                argument.category.toLowerCase() === events.target.value
-            )
-            .map((card) => enabledCard(card))
-            .join("");
-          active.push(create);
-          homeContainer.innerHTML = create;
-        } else {
-          let inactive = active.indexOf(events.target.value);
-          if (inactive != -1) {
-            active.splice(inactive, 1);
-            homeContainer.innerHTML = create;
-          }
-          homeContainer.innerHTML = homeCards;
+    //------------------------------------------------------------ Rendering Functions -------------------------------------------------------//
+    function createCategory(event) {
+      let genericCategory = "";
+      for (let i of event) {
+        genericCategory += `
+          <label id="label-switch" for="category[]" class="form-check-label ms-4 mt-3">
+            <input class="check-box form-check-input" type="checkbox" name="${i}" id="${i}" value=${i}> ${i}
+          </label>
+          `;
+      }
+      homeCategory.innerHTML = genericCategory;
+    }
+    function homeCards(arrayData) {
+      let genericCardUp = "";
+      for (let array of arrayData) {
+        genericCardUp += `<div class="card-scale ms-5 mt-5 d-flex flex-wrap">
+              <div class="card" style="width: 14rem">
+                <img
+                  src="${array.image}"
+                  class="card-img m-1"
+                  alt="img-food"
+                />
+                <div class="card-body">
+                  <h5 class="card-title text-center">${array.name}</h5>
+                  <p class="card-description text-center">${array.description}</p>
+                  <a
+                    href="./details.html?id=${array._id}"
+                    class="btn btn-outline-danger details-boton"
+                    id="food-boton"
+                    >More Details</a
+                  >
+                </div>
+              </div>
+            </div>`;
+      }
+      return genericCardUp;
+    }
+    function filterFunction() {
+      searchFilter = homeData.events.filter(
+        (arrayFilter) =>
+          arrayFilter.category.toLowerCase().includes(propertySearch) ||
+          arrayFilter.name.toLowerCase().includes(propertySearch) ||
+          arrayFilter.date.includes(propertySearch)
+      );
+
+      categoryCheck = homeData.events.filter((aEvent) =>
+        checked.includes(aEvent.category)
+      );
+
+      if (searchFilter.length > 0) {
+        homeContainer.innerHTML = homeCards(searchFilter);
+        let arrayActive = searchFilter.filter((event) =>
+          event.category.includes(checked.toString())
+        );
+        homeContainer.innerHTML = homeCards(arrayActive);
+      } else if (searchFilter == 0) {
+        homeContainer.innerHTML = `<div class="card-scale ms-5 mt-5 d-flex flex-wrap">
+        <div class="card" style="width: 14rem">
+          <img
+            id="error-img"
+            src="./assets/images/lupa-x.jpg"
+            class="card-img m-1"
+            alt="img-food"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center">Card Not Found</h5>
+            <p class="card-description text-center"></p>
+            <a
+              href="./index.html"
+              class="btn btn-outline-danger details-boton"
+              id="error-boton"
+              >Return to home</a
+            >
+          </div>
+        </div>
+        </div>`;
+      }
+      if (categoryCheck.length > 0) {
+        homeContainer.innerHTML = homeCards(categoryCheck);
+        let arrayNotActive = categoryCheck.filter((arrayNot) =>
+          arrayNot.name.toLowerCase().includes(propertySearch.toString())
+        );
+        homeContainer.innerHTML = homeCards(arrayNotActive);
+        if (searchFilter == 0) {
+          homeContainer.innerHTML = `<div class="card-scale ms-5 mt-5 d-flex flex-wrap">
+        <div class="card" style="width: 14rem">
+          <img
+            id="error-img"
+            src="./assets/images/lupa-x.jpg"
+            class="card-img m-1"
+            alt="img-food"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center">Card Not Found</h5>
+            <p class="card-description text-center"></p>
+            <a
+              href="./index.html"
+              class="btn btn-outline-danger details-boton"
+              id="error-boton"
+              >Return to home</a
+            >
+          </div>
+        </div>
+        </div>`;
         }
       }
-
-      console.log(events.target.checked);
-    });
+    }
+    createCategory(categoryFiltering);
+    filterFunction();
   } catch (error) {
     console.log(error);
   }
 }
-fetchData();
 
-//---------------------------------------------------------------- Home Cards ----------------------------------------------------------------//
-
-function enabledCard(array) {
-  genericCard = `<div class="card-scale ms-5 mt-5 d-flex flex-wrap">
-  <div class="card bg-body-tertiary" style="width: 14rem">
-    <img
-      src="${array.image}"
-      class="card-img m-1"
-      alt="img-food"
-    />
-    <div class="card-body">
-      <h5 class="card-title text-center">${array.name}</h5>
-      <p class="card-description text-center">${array.description}</p>
-      <a
-        href="./details.html?id=${array._id}"
-        class="btn btn-outline-danger details-boton"
-        id="details-boton"
-        >More Details</a
-      >
-    </div>
-  </div>
-</div>`;
-  return genericCard;
-}
-//------------------------------------------------------------------ Search ------------------------------------------------------------------//
-
-const wrongCard = `<div class="card-scale ms-5 mt-5 d-flex flex-wrap">
-<div class="card" style="width: 14rem">
-  <img
-    id="error-img"
-    src="./assets/images/lupa-x.jpg"
-    class="card-img m-1"
-    alt="img-food"
-  />
-  <div class="card-body">
-    <h5 class="card-title text-center">Card Not Found</h5>
-    <p class="card-description text-center"></p>
-    <a
-      href="./index.html"
-      class="btn btn-outline-danger details-boton"
-      id="error-boton"
-      >Return to home</a
-    >
-  </div>
-</div>
-</div>`;
-
-//-------------------------------------------------------------- Category CheckBox -----------------------------------------------------------//
-
-function enableCheckbox(array) {
-  genericCheckbox = `<div class="form-check form-check-inline p-3 ms-2">
-  <input
-    class="form-check-input"
-    type="checkbox"
-    id="${array.category.toLowerCase()}-checkbox"
-    value="${array.category.toLowerCase()}"
-  />
-  <label class="form-check-label" for="homeCheckbox1"
-    >${array.category}</label
-  >
-</div>`;
-  return genericCheckbox;
-}
+fetchDataHome();
